d6f1fc9c216371fb22aeb1f4e0f32c1053d2bc24
Luyen1608 <96036620+Luyen1608@users.noreply.github.com>
2025-01-13 22:25:20 +0700

    update

---
+++ assets/js/common.js
@@ -0,0 +1,7 @@
+AOS.init({
+    duration: 1200, // Thời gian mặc định cho tất cả các animation
+    easing: 'ease-in-out', // Easing mặc định
+    offset: 100, // Khoảng cách mặc định trước khi kích hoạt animation
+    delay: 100, // Độ trễ mặc định
+    once: true, // Animation chỉ chạy 1 lần
+  });
